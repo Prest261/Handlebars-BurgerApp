3cf0a245f2483a012d806919ff26347c4f982998
Syd Rigel <prest261@umn.edu>
2020-02-23 19:13:59 -0700

    installed all the initial dependencies, and created db file

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Mushroom & Swiss');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Cheeseburger');
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('BBQ & Cheddar', true);
